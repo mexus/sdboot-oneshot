9ce626b90ce5784a5a9031d2ddb245adfec3a950
mexus <null@mexus.xyz>
2022-01-14 02:01:50 +0300

    Null-terminated utf16 strings before write

--- sdboot/src/write.rs
+++ sdboot/src/write.rs
@@ -5,7 +5,8 @@ use efivar::{
 };
 
 /// Converts the provided string into a UTF-16 representation and writes it to
-/// the given EFI variable.
+/// the given EFI variable, adding terminating null bytes if the original string
+/// is not null-terminated.
 pub fn write_utf16_string<T: VarWriter + ?Sized>(
     var_manager: &mut T,
     name: &VariableName,
@@ -15,8 +16,17 @@ pub fn write_utf16_string<T: VarWriter + ?Sized>(
     let mut buffer = Vec::with_capacity(value.as_bytes().len() * 2);
     for wide_char in value.encode_utf16() {
         let [first, second] = wide_char.to_le_bytes();
+        if first == 0 && second == 0 {
+            log::warn!("Intermediate null byte in {:?}", value);
+        }
         buffer.push(first);
         buffer.push(second);
+    }
+    if let [.., 0, 0] = &buffer[..] {
+        // Already null-terminated. No need to add trailing zeroes.
+    } else {
+        // Not null-terminated. Add trailing zeroes!
+        buffer.extend_from_slice(&[0, 0]);
     }
     log::trace!("{} encoded as utf16 {:x?}", value, buffer);
 
